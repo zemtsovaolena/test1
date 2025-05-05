74de27cfa3e63b5561244397c46e6f4c1e5a4d35
zemtsovaolena <sale.gorka@gmail.com>
2025-05-05 13:43:02 +0200

    Initial Commit

---
+++ test1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test1
+//
+//  Created by Olena Zemtsova on 05/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Olena Zemtsova on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
