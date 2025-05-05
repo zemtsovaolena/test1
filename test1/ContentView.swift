b330e903318495933cc8df821165118097b77017
zemtsovaolena <sale.gorka@gmail.com>
2025-05-05 13:55:46 +0200

    woods new

--- test1/ContentView.swift
+++ test1/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, you and I!")
+            Image("woods")
+                .resizable()
         }
         .padding()
     }
